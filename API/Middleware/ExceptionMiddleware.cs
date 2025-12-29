f0ada42d02c40eebb76ad4866e7e89d855f303e4
Darichel Perez <darichel.perez@gmail.com>
2025-12-29 17:55:56 -0300

    Feat/Server, exception middleware

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,56 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (ValidationException ex)
+        {
+            await HandleValidationException(context, ex);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private async Task HandleValidationException(HttpContext context, ValidationException ex)
+    {
+        var validationErrors = new Dictionary<string, string[]>();
+
+        if (ex.Errors is not null)
+        {
+            foreach (var error in ex.Errors)
+            {
+                if (validationErrors.TryGetValue(error.PropertyName, out var existeingErrors))
+                {
+                    validationErrors[error.PropertyName] = existeingErrors.Append(error.ErrorMessage).ToArray();
+                }
+                else
+                {
+                    validationErrors[error.PropertyName] = [error.ErrorMessage];
+                }
+            }
+        }
+        
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        var validationProblemsDetails = new ValidationProblemDetails(validationErrors)
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Type = "ValidationFailure",
+            Title = "Validation Error",
+            Detail = "One or more validation errors occurred"
+        };
+        
+        await context.Response.WriteAsJsonAsync(validationProblemsDetails);
+    }
+}
