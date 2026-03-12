7b86ff8a60b1a820a9a2d0af635f7e493c57f781
Darichel Perez <darichel.perez@gmail.com>
2026-03-12 14:41:39 -0300

    feat/ Client, create a login form

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,7 +9,7 @@ namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-    [AllowAnonymous]
+
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
