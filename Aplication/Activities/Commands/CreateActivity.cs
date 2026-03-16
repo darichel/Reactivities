27d539f4905296069fce4689334b4c865c0b1e59
Darichel Perez <darichel.perez@gmail.com>
2026-03-16 15:07:55 -0300

    feat/ Server: updating the created activity

--- Aplication/Activities/Commands/CreateActivity.cs
+++ Aplication/Activities/Commands/CreateActivity.cs
@@ -1,5 +1,6 @@
 using Aplication.Activities.DTOs;
 using Aplication.Core;
+using Aplication.Interfaces;
 using AutoMapper;
 using Domain;
 using FluentValidation;
@@ -15,18 +16,29 @@ public class CreateActivity
         public required CreateActivityDto ActivityDto { get; set; }
     }
 
-    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, Result<string>>
+    public class Handler(AppDbContext context, IMapper mapper, IUserAccessor userAccessor)
+        : IRequestHandler<Command, Result<string>>
     {
         public async Task<Result<string>> Handle(Command request, CancellationToken cancellationToken)
         {
+            var user = await userAccessor.GetUserAsync();
             
             var activity = mapper.Map<Activity>(request.ActivityDto);
-            
+
             context.Activities.Add(activity);
+
+            var attendee = new ActivityAttendee
+            {
+                ActivityId = activity.Id,
+                UserId = user.Id,
+                IsHost = true,
+            };
             
+            activity.Attendees.Add(attendee);
+
             var result = await context.SaveChangesAsync(cancellationToken) > 0;
-            
-            if(!result) return Result<string>.Failure("Failed to create activity", 400);
+
+            if (!result) return Result<string>.Failure("Failed to create activity", 400);
 
             return Result<string>.Success(activity.Id);
         }
