5f1e388606e131ece92543da68da84b21d833631
Darichel Perez <darichel.perez@gmail.com>
2026-03-16 08:14:16 -0300

    feat/ Client, create an user menu

--- Client/src/features/activities/dashboard/ActivityList.tsx
+++ Client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,21 +1,18 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import ActivityCard from "./ActivityCard";
 import { useActivities } from "../../../lib/hooks/useActivities";
 
-
-
 export default function ActivityList() {
-  const { activities, isPending } = useActivities();
+  const { activities, isLoading } = useActivities();
 
-  if(!activities || isPending) return <div>Loading activities...</div>;
+  if (isLoading) return <Typography>Loading activities...</Typography>;
+
+  if (!activities) return <Typography>No activities found</Typography>;
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
       {activities.map((activity) => (
-        <ActivityCard
-          key={activity.id}
-          activity={activity}
-
-        />
+        <ActivityCard key={activity.id} activity={activity} />
       ))}
     </Box>
   );
